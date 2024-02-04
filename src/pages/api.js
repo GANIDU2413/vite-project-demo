ef5d7a89f8e6a03d49bb4b6d94ab595894a259e3
GANIDU2413 <ganidusahan@gmail.com>
2024-02-04 12:52:33 +0530

    0402241252

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,18 +1,24 @@
 import axios from "axios";
 
+const BASE_API = axios.create({
+    baseURL:"https://jsonplaceholder.typicode.com",
+    timeout:3000,
+    headers:{},
+});
+
 export async function getBlogPost() {
-    const _res = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+    const _res = await BASE_API.get("/posts/1");
     return _res;
 }
 
 export async function createBlogPost(){
-    const _res = await axios.post('https://jsonplaceholder.typicode.com/posts',
+    const _res = await BASE_API.post('/posts',
     {title:'this is title',body:'this is body',userID: 12}
     );
     return _res;
 }
 
 export async function deleteBlogPost(){
-    const _res = await axios.delete('https://jsonplaceholder.typicode.com/posts/1');
+    const _res = await BASE_API.delete('/posts/1');
     return _res;
 }
